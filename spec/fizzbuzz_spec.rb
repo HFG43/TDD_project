b1072104a3735350965ea73f1121b3252b2a86c2
francksefu <francksefu1998@gmail.com>
2023-09-08 22:18:56 +0200

    First test for fizzbuzz method failed'

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,4 +1,11 @@
 require 'rspec'
+require_relative '../solver.rb'
 
 describe Solver do
+	describe '#fizzbuzz' do
+		it 'should take an argument, an integer, and return a string' do
+			obj_class = Solver.new
+			expect(obj_class.fizzbuzz(9)).to eq('fizz')
+		end
+	end
 end
