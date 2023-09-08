730281dfdc81704cfedc4816cf8cd6d4084b91a5
francksefu <francksefu1998@gmail.com>
2023-09-08 22:30:47 +0200

    Add other test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,9 +3,24 @@ require_relative '../solver.rb'
 
 describe Solver do
 	describe '#fizzbuzz' do
-		it 'should take an argument, an integer, and return a string' do
+		it 'should take an argument is multiple of 3, an integer, and return a fizz' do
 			obj_class = Solver.new
 			expect(obj_class.fizzbuzz(9)).to eq('fizz')
+		end
+
+		it 'should take an argument is multiple of 5, an integer, and return a buzz' do
+			obj_class = Solver.new
+			expect(obj_class.fizzbuzz(10)).to eq('buzz')
+		end
+
+		it 'should take an argument is multiple of 15, an integer, and return a fizzbuzz' do
+			obj_class = Solver.new
+			expect(obj_class.fizzbuzz(15)).to eq('fizzbuzz')
+		end
+
+		it 'should take an argument is multiple of 7, an integer, and return a 7' do
+			obj_class = Solver.new
+			expect(obj_class.fizzbuzz(7)).to eq('fizz')
 		end
 	end
 end
