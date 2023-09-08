13efcf3d7b8c2e39ed674af269d487f4d2f02dfa
HFG43 <hernanguemes@hotmail.com>
2023-09-08 15:31:55 -0300

    Chore: addapt factorial to recive input from class

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,22 +1,21 @@
-# require 'spec_helper'
-# requier_relative '../solver.rb'
+require 'spec_helper'
+requier_relative '../solver.rb'
 
-describe 'factorial' do
-  it "it should return the integer factorized" do
-    integer = 5
-    result = factorial(integer)
-    expect (result).to eq(120)
-  end
+describe Solver do
+  describe '#factorial' do
+    it "it should return the integer factorized" do
+      result = Solver.new.factorial(5)
+      expect(result).to eq(120)
+    end
   
-  it "should return 1 the factorization of integer zero" do
-    integer = 0
-    zero_result = factorial(integer)
-    expect (zero_result).to eq(1)
-  end
+    it "should return 1 the factorization of integer zero" do
+      result = Solver.new.factorial(0)
+      expect(result).to eq(1)
+    end
 
-  it "should raise an exeption if is a negative number"
-    integer = -5
-    negative_integer_result = factorial(integer)
-    expect { factorial(negative_integer_result)}.to raise_error('Error')
+    it "should raise an exeption if is a negative number"
+      result = Solver.new.factorial(-5)
+      expect(result).to raise_error('Error')
+    end
   end
-end
+end  
